434afb91ff837537a5c88e44a0254daaf0819da6
Asanatsa <me@asanatsa.cc>
2023-10-27 01:15:03 +0800

    update file

---
+++ generator.js
@@ -0,0 +1,99 @@
+let emojiList = [
+    {
+        name: "傻不啦叽诗歌剧",
+        id: "sblj",
+        description: "©祭祭",
+        imgList: [
+            {
+                name: "乐了",
+                id: "happy",
+                src: "./shabulaji/happy.png"
+            },
+            {
+                name: "大悲",
+                id: "sad",
+                src: "./shabulaji/sad.png"
+            },
+            {
+                name: "困",
+                id: "sleepy",
+                src: "./shabulaji/sleepy.png"
+            },
+            {
+                name: "惊",
+                id: "shocked",
+                src: "./shabulaji/shocked.png"
+            },
+            {
+                name: "变傻",
+                id: "silly",
+                src: "./shabulaji/silly.png"
+            },
+            {
+                name: "哭哭",
+                id: "cry",
+                src: "./shabulaji/cry.png"
+            },
+            {
+                name: "羞",
+                id: "shy",
+                src: "./shabulaji/shy.png"
+            },
+            {
+                name: "佛性",
+                id: "fo'xing",
+                src: "./shabulaji/fo'xing.png"
+            }, 
+        ],
+    },
+    {
+        name: "乌拉拉",
+        id: "urara",
+        description: "©祭祭",
+        imgList: [
+            {
+                name: "忍住",
+                id: "endure",
+                src: "./urara/endure.png"
+            },
+            {
+                name: "打call",
+                id: "supported",
+                src: "./urara/suppored.png"
+            },
+            {
+                name: "打瞌睡",
+                id: "sleepy",
+                src: "./urara/sleepy.png"
+            },
+            {
+                name: "哇哦",
+                id: "wow",
+                src: "./urara/wow.png"
+            },
+            {
+                name: "哼哼",
+                id: "huh",
+                src: "./urara/huh.png"
+            },
+            {
+                name: "乐",
+                id: "lol",
+                src: "./urara/lol.png"
+            },
+            {
+                name: "切",
+                id: "so_what",
+                src: "./urara/so_what.png"
+            },
+            {
+                name: "伤心",
+                id: "sad",
+                src: "./urara/sad.png"
+            }, 
+        ]
+    },
+]
+
+
+
